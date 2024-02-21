febdd32d43022bcc047491b5006eff90602ad581
tony0001234 <tony0001234@gmail.com>
2024-02-20 19:29:31 -0600

    inicio de creacion del menu

--- Tarea 03/Tarea03.cpp
+++ Tarea 03/Tarea03.cpp
@@ -6,6 +6,18 @@ using namespace std;//importa todo el espacio de nombres std al codigo actual, s
 
 /*Autor: Anthony Fabian Ramirez Orellana. Nombre del programa: Metodo de de punto fijo*/
 
+void menu(){
+	cout<< "" << endl;//imprimo en la consola todo el menu, con saltos de linea para que se vea bonito.
+	cout<<"Ingrese la opcion que desea ejecutar:" << endl;
+	cout<<"0) Regresar a este menu." << endl;
+	cout<<"1) f(x) = x^4 -3x^2 -3 = 0 con transfomacion a: x = ({3/[(x^2)-3]}^1/2)  tolerancia: 10^-5" << endl;
+	cout<<"2) f(x) = x^4 -3x^2 -3 = 0 con transfomacion a: x = ({3/[(x^2)-3]}^1/2)  tolerancia: 10^-10" << endl;
+	cout<<"3) f(x) = x^4 -3x^2 -3 = 0 con transfomacion a: x = {[(3x^2)+3]^1/4}  tolerancia: 10^-5" << endl;
+	cout<<"4) f(x) = x^4 -3x^2 -3 = 0 con transfomacion a: x = {[(3x^2)+3]^1/4}  tolerancia: 10^-10" << endl;
+	
+	cout<<"5) Salir.";
+}
+
 void cabezaTabla(){//creo un proceso de tipo vacio, ya que no necesito que me de una respuesta como una funcion, sino que realize una porcion de codigo, enviandole a la propia funcion ningun parametro, ya que solo pintara la consola.
 	system("cls");//codigo del cmd para limpiar la consola 
 	cout<<"-----------------------------------------------------------------------------------------------------------------------"<< endl;//cout se utiliza para imprimir en consola el contenido dentro de los parentesis.
