0cb69adca353d892972fc36cf623a6e5982a07bc
tony0001234 <tony0001234@gmail.com>
2024-04-25 19:30:50 -0600

    terminado

--- Tarea 10/Diferencias divididas.cpp
+++ Tarea 10/Diferencias divididas.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <vector>
+#include <iostream>//incluyo la libreria iostream, para poder tener acceso a los dispositivos de entrada y salida
+#include <vector>//Es una platilla de clase que proporciona un contenedor de memoria dinamica, puede contener un numero variable de elementos del mismo tipo
 
 using namespace std;//importa todo el espacio de nombres std al codigo actual, se utiliza mayormente para utilizar el prefijo std::
 
 // Función para calcular las diferencias divididas
-vector<vector<double>> diferencias_divididas(const vector<double>& x, const vector<double>& y) {
-    int n = x.size();
-    vector<vector<double>> tabla(n, vector<double>(n, 0.0));
+vector<vector<double>> diferencias_divididas(const vector<double>& x, const vector<double>& y) {//creo una funcion de el tipo vector variable tipo dobule con nombre diferencias_divididas y le paso como parametros el vector "x" y el "y" constantes para no modificarlos la calcular
+    int n = x.size();//creo la variable n igualada a la cantidad de valores en x, el tamaño de x
+    vector<vector<double>> tabla(n, vector<double>(n, 0.0));//cre un vector variable tipo double llamado tabla pasandole el valor de n y el de vector double en posicion n, 0.0
 
-    // Copiar y a la primera columna de la tabla
+    // Copiar "y" a la primera columna de la tabla
     for (int i = 0; i < n; ++i) {
         tabla[i][0] = y[i];
     }
@@ -20,31 +20,31 @@ vector<vector<double>> diferencias_divididas(const vector<double>& x, const vect
         }
     }
 
-    return tabla;
+    return tabla;//para trabajr con la siguiente funcion
 }
 
 // Función para realizar la interpolación de Newton
-double newton_interpolacion(const vector<double>& x, const vector<double>& y, double valor) {
-    vector<vector<double>> tabla = diferencias_divididas(x, y);
-    int n = x.size();
-    double interpolacion = tabla[0][0];
+double newton_interpolacion(const vector<double>& x, const vector<double>& y, double valor) {//esta funcion es de tipo double y paso los valros constantes del vector variable tipo dobule "x" y el "y", tambien el valor a interpolar
+    vector<vector<double>> tabla = diferencias_divididas(x, y);//creo mi vector variable tipo double de nombre tabla y lo igualo a la funcion anterior para calcular los valores
+    int n = x.size();//creo n de nuevo del tamaño de x para la manipulacion de los for
+    double interpolacion = tabla[0][0];//creo la variable double interpolacon para guardar los resultados
 
-    cout << "Valor A0"<<": "<<tabla[0][0]<<endl;
+    cout << "Valor A0"<<": "<<tabla[0][0]<<endl;//despliego el primero que es fijo de la tabla entregada por el usuario
 
-    for (int i = 1; i < n; ++i) {
-        double termino = tabla[0][i];
-        cout << "Valor A"<<i<<": "<<termino<<endl;
-        for (int j = 0; j < i; ++j) {
-            termino *= (valor - x[j]);
+    for (int i = 1; i < n; ++i) {//inicio con los for para calcular los valores, desde 1 ya que el anterior era el 0
+        double termino = tabla[0][i];//creo mi variable termino para almacenar el valor cada vez que se regrese en el for
+        cout << "Valor A"<<i<<": "<<termino<<endl;//despliego el valor en consola para mostrar los distintos A
+        for (int j = 0; j < i; ++j) {//comienzo con el siguiente for, esta vez con j en cero, sienfo j menor que i
+            termino *= (valor - x[j]);// calculo termino = tremino * (valor - x[j]), solamente acortando un poco la linea con =*
         }
-        interpolacion += termino;
+        interpolacion += termino;// sumo interpolacion = interpolacion + termino, para ir calculando el valor final
     }
 
-    return interpolacion;
+    return interpolacion;//retorno interpolacion para mostrar el valor interpolado
 }
 
 int main() {
-    int n = 0;
+    int n = 0;//creo una variable entera n para pedir el numero total de valores en la tabla
     // Puntos conocidos para la interpolación
     cout<<"ingrese el numero de filas: "<<endl;//despliego un mensaje para pedir el valor de filas, utilizando n
     try//abro un try and catch para prevenir ingreso de datos erroneos
@@ -53,9 +53,9 @@ int main() {
     }
     catch(const invalid_argument& e)//de aber un error la aplicacion no calculara nada y se cerrara
     {
-        cout << e.what() << '\n';
+        cout << e.what() << '\n';//despliego en consola que hubo un error en la ejecucion
     }
-    vector<double> x(n);
+    vector<double> x(n);//creo 2 vectores con tamaño variable, les paso de una vez su valor de numero de datos
     vector<double> y(n);
 
     cout<<"Ingrese los valores Xs, Ys"<<endl;//doy contexto de los valores que pedire a continuacion.
@@ -72,7 +72,7 @@ int main() {
                 }
                 catch(const std::invalid_argument& e)//de aber un error la aplicacion no calculara nada y se cerrara
                 {
-                    cout << e.what() << '\n';
+                    cout << e.what() << '\n';//despliego en consola que hubo un error en la ejecucion
                 }
             }
         }else {
@@ -86,7 +86,7 @@ int main() {
                 }
                 catch(const std::invalid_argument& e)//de aber un error la aplicacion no calculara nada y se cerrara
                 {
-                    cout << e.what() << '\n';
+                    cout << e.what() << '\n';//despliego en consola que hubo un error en la ejecucion
                 }
             }
         }
@@ -101,14 +101,14 @@ int main() {
     }
     catch(const invalid_argument& e)//de aber un error la aplicacion no calculara nada y se cerrara
     {
-        cout << e.what() << '\n';
+        cout << e.what() << '\n';//despliego en consola que hubo un error en la ejecucion
     }
 
 
-    system("cls");
+    system("cls");//utilizo system para limpiar la consola de la recopilacion de datos
     // Realizar la interpolación y mostrar el resultado
-    double resultado = newton_interpolacion(x, y, valor_a_interpolar);
-    cout << "El valor interpolado en " << valor_a_interpolar << " es aproximadamente: " << resultado << std::endl;
+    double resultado = newton_interpolacion(x, y, valor_a_interpolar);//creo una variable resultado y la igualo a mi funcion para iniciar con todos los calculos, mandando los vectores "x", "y" y el valor a interpolar
+    cout << "El valor interpolado en " << valor_a_interpolar << " es aproximadamente: " << resultado << std::endl;//despliego el resultado en consola
     system("pause");//utilizo sistem para pausar el programa
     return 0;
 }
